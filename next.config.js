f2d6f79cf6e2f521f75cecf0f891fd1315a3257e
“bazzeff” <bazzeffanu@gmail.com>
2022-01-04 15:26:46 -0500

    github Pages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,24 @@
-module.exports = {
-  reactStrictMode: true,
-}
+//module.exports = {
+//  assetPrefix: !debug ? '/my-nextjs-app/' : '',
+//  reactStrictMode: true,
+//} 
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+module.exports = withPlugins([
+  [optimizedImages, {
+    mozjpeg: {
+      quality: 80,
+    },
+    pngquant: {
+      speed: 3,
+      strip: true,
+      verbose: true,
+    },
+    imagesPublicPath: '/my-nextjs-app/_next/static/images/',
+  }],
+  {
+    basePath: '/my-nextjs-app',
+    assetPrefix: '/my-nextjs-app/',
+    env,
+  },
+]);
